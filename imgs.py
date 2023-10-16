a8545d6bd1c5cbebf29904f4531043458769901e
Beda Schmid <beda@tukutoi.com>
2023-10-16 15:05:20 +0700

    Support Leo, async img download

--- imgs.py
+++ imgs.py
@@ -1,6 +1,7 @@
 import os
-import requests
 import json
+import aiohttp
+import asyncio
 
 n = 0
 save_folder = "imgs"
@@ -9,18 +10,25 @@ os.makedirs(save_folder, exist_ok=True)
 with open('logs.json', 'r') as file:
     json_array = json.load(file)
 
-def download_image(url, file_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Image downloaded: {file_path}")
-    else:
-        print(f"Failed to download image: {url}")
+async def download_image(url, file_path):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                with open(file_path, 'wb') as file:
+                    file.write(await response.read())
+                print(f"Image downloaded: {file_path}")
+            else:
+                print(f"Failed to download image: {url}")
 
-for item in json_array:
-    image_url = item['url']
-    file_name = str(n) + "-image.png"  # Extract file name from the URL
-    file_path = os.path.join(save_folder, file_name)  # Create the file path
-    download_image(image_url, file_path)
-    n += 1
+async def main():
+    global n
+    tasks = []
+    for item in json_array:
+        image_url = item['url']
+        file_name = str(n) + "-image.png"  # Extract file name from the URL
+        file_path = os.path.join(save_folder, file_name)  # Create the file path
+        tasks.append(download_image(image_url, file_path))
+        n += 1
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
-from dotenv import load_dotenv
-import json
-import openai
 import sys
+import json
+import time
+import requests
+import openai
+from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-assistant = "You are a comic writer. You follow up to the previous comic panel scene with a new comic panel scene. Your response includes only visual descriptions of the comic panel scene itself. You use about 120 characters for the description. Your next comic panel scene is a direct continuation of this scene: "
-prompt = input("Enter prompt:" )
-artstyle = input("Enter Artstyle (Example: \"illustrated by Juan Giménez\", or \"in black and white pencil drawing style\"):")
-panel_nu = int( input("Enter amount of Panels to generate:") )
+assistant = (
+    "You are a comic writer. You follow up to the previous comic panel scene with a new comic panel scene. "
+    "Your response includes only visual descriptions of the comic panel scene itself. "
+    "You use about 120 characters for the description. Your next comic panel scene is a direct continuation of this scene: "
+)
+prompt = input("Enter prompt: ")
+artstyle = input(
+    "Enter Artstyle (Example: \"illustrated by Juan Giménez\", or \"in black and white pencil drawing style\"): "
+)
+panel_nu = int(input("Enter amount of Panels to generate: "))
 
 base_url = os.getenv("BASE_URL")
 model = os.getenv("MODEL")
@@ -21,6 +29,9 @@ openai.api_key = os.getenv("TOKEN")
 jsonpath = "logs.json"
 cpath = "comic.pdf"
 panel_n = 1
+leo_url = os.getenv("LEO_URL")
+leo_token = os.getenv("LEO_TOKEN")
+
 
 def appendto(file, data):
     if os.path.isfile(file) and os.path.getsize(file) > 0:
@@ -32,58 +43,107 @@ def appendto(file, data):
     with open(file, 'w') as f:
         json.dump(existing_data, f)
 
+
 def make_image(result):
     image = openai.Image.create(
-		prompt= f"An intrinsic detailed single comic panel scene {artstyle} showing " + result,
-		n=1,
-		size="512x512"
-	)
+        prompt=f"An intrinsic detailed single comic panel scene {artstyle} showing " + result,
+        n=1,
+        size="512x512"
+    )
     image_url = image['data'][0]['url']
     data = {
-		"content": result,
-		"url": image_url
-	}
+        "content": result,
+        "url": image_url
+    }
     appendto(jsonpath, data)
+
+
+def make_leo_image(result):
+    headers = {
+        "accept": "application/json",
+        "authorization": "Bearer " + leo_token,
+        "content-type": "application/json"
+    }
+    data = {
+        "height": 512,
+        "modelId": "ac614f96-1082-45bf-be9d-757f2d31c174",
+        "prompt": result + " " + artstyle,
+        "width": 912,
+		"alchemy": False,
+		"controlNet": False,
+		"guidance_scale": 7,
+		"highContrast": True,
+		"highResolution": True,
+		"nsfw": False,
+		"num_images": 1,
+		"photoReal": False,
+		"promptMagic": True,
+		"public": False,
+		"seed": 123456789,
+		"tiling": False,
+		"unzoom": False,
+		"promptMagicVersion": "v2"
+    }
+    post_response = requests.post(leo_url, headers=headers, json=data)
+    post_response_data = post_response.json()
+    generation_id = post_response_data['sdGenerationJob']['generationId']
+    time.sleep(5)
+    max_retries = 10
+    retry_interval = 10  # in seconds
+    get_url = f"https://cloud.leonardo.ai/api/rest/v1/generations/{generation_id}"
+    for _ in range(max_retries):
+        get_response = requests.get(get_url, headers=headers)
+        get_response_data = get_response.json()
+        status = get_response_data['generations_by_pk']['status']
+        if status == "COMPLETE":
+            image_url = get_response_data['generations_by_pk']['generated_images'][0]['url']
+            break
+        else:
+            time.sleep(retry_interval)
+    data = {
+        "content": result,
+        "url": image_url
+    }
+    appendto(jsonpath, data)
+
 
 def get_legacy_response(prompt, n, panel_n, panel_nu):
     if panel_n == panel_nu:
         end_message = 'All panels have been generated. Run imgs.py and then comic.py to finish your Comic!'
-        print( end_message )
+        print(end_message)
         sys.exit()
     if n == 0:
-        make_image(prompt)
-        prompt = assistant + prompt
+        make_leo_image(prompt)
+		# make_image(prompt)
+    prompt = assistant + prompt
     try:
         response = openai.Completion.create(
-			model=model,
-			prompt=prompt,
-			temperature=temp,
-			max_tokens=max_tokens,
-			frequency_penalty=freq,
-			presence_penalty=pres
-		)
+            model=model,
+            prompt=prompt,
+            temperature=temp,
+            max_tokens=max_tokens,
+            frequency_penalty=freq,
+            presence_penalty=pres
+        )
         result = response['choices'][0]['text']
         completion_tokens = None
         if 'usage' in response and 'completion_tokens' in response['usage']:
             completion_tokens = response['usage']['completion_tokens']
         if not completion_tokens or completion_tokens == 0:
             get_legacy_response(prompt, n, panel_n, panel_nu)
-
-        make_image(result)
-
+        make_leo_image(result)
         n += 1
-        prompt = assistant + result
         panel_n += 1
-
-        get_legacy_response(prompt, n, panel_n, panel_nu)
-
+        get_legacy_response(result, n, panel_n, panel_nu)
         return result
     except Exception as e:
         result = f'Request failed with exception {e}'
-        print( result )
+        print(result)
         return False
+
 
 def recur(panel_n, panel_nu):
     get_legacy_response(prompt, 0, panel_n, panel_nu)
+
 
 recur(panel_n, panel_nu)
